8513b50fed277554a268a7af950decdc79e6e796
Victor Vargas <vvargasnunez@gmail.com>
2020-09-04 14:07:59 -0700

    Complete binary agent exercise

---
+++ binaryAgents.js
@@ -0,0 +1,11 @@
+// Binary Agents - Return an English translated sentence of the passed binary string. The binary string will be space separated.
+
+// binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111") should return "Aren't bonfires fun!?"
+
+function binaryAgent(str) {
+  let arr = str.split(' ');
+  arr = arr.map(x => {
+    return String.fromCharCode(parseInt(x, 2));
+  });
+  return arr.join('');
+}
--- steamroller.js
+++ steamroller.js
@@ -1,5 +1,10 @@
 // Steamroller - Flatten a nested array. You must account for varying levels of nesting.
 
+// steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+// steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
+// steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
+// steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
+
 function steamrollArray(arr) {
   let newArr = [];
   for(let i = 0; i < arr.length; i++){
